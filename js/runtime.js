b2a21b763700b70f58442043c225f3034107d8b0
askdk3 <zhguzibo@163.com>
2022-09-25 14:04:09 +0800

    Site updated: 2022-09-25 14:04:09

--- js/runtime.js
+++ js/runtime.js
@@ -1,12 +1,1 @@
-// 底部本站运行时间
-var now=new Date;
-function createtime(){
-    var grt=new Date("09/16/2022 00:00:00");
-    now.setTime(now.getTime()+250);
-    var days=(now-grt)/1e3/60/60/24,dnum=Math.floor(days),hours=(now-grt)/1e3/60/60-24*dnum,hnum=Math.floor(hours);1==String(hnum).length&&(hnum="0"+hnum);
-    var minutes=(now-grt)/1e3/60-1440*dnum-60*hnum,mnum=Math.floor(minutes);1==String(mnum).length&&(mnum="0"+mnum);
-    var seconds=(now-grt)/1e3-86400*dnum-3600*hnum-60*mnum,snum=Math.round(seconds);1==String(snum).length&&(snum="0"+snum);let currentTimeHtml="";
-    currentTimeHtml=`
-    <span class='textTip'>  本站居然运行了 ${dnum} 天</span>
-    <span id='runtime'> ${hnum} 小时 ${mnum} 分 ${snum} 秒 </span> <i class='fas fa-heartbeat' style='color:red'></i>`,document.getElementById("workboard")&&(document.getElementById("workboard").innerHTML=currentTimeHtml)}setInterval(()=>{createtime()},250);
-
+var now=new Date;function createtime(){var e=new Date("09/16/2022 00:00:00");now.setTime(now.getTime()+250);var t=(now-e)/1e3/60/60/24,n=Math.floor(t),a=(now-e)/1e3/60/60-24*n,r=Math.floor(a);1==String(r).length&&(r="0"+r);var o=(now-e)/1e3/60-1440*n-60*r,i=Math.floor(o);1==String(i).length&&(i="0"+i);var l=(now-e)/1e3-86400*n-3600*r-60*i,s=Math.round(l);1==String(s).length&&(s="0"+s);let w="";w=`\n    <span class='textTip'>  本站居然运行了 ${n} 天</span>\n    <span id='runtime'> ${r} 小时 ${i} 分 ${s} 秒 </span> <i class='fas fa-heartbeat' style='color:red'></i>`,document.getElementById("workboard")&&(document.getElementById("workboard").innerHTML=w)}setInterval((()=>{createtime()}),250);

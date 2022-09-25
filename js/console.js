395408f66934408a19dbae7ecac03badff931afd
askdk3 <zhguzibo@163.com>
2022-09-25 14:07:46 +0800

    Site updated: 2022-09-25 14:07:46

--- js/console.js
+++ js/console.js
@@ -1,18 +1,1 @@
-// console.clear();  // 清屏
-console.log(`
-    ██████████    █████╗    ██╗       ██████████╗ 
-    ██║          ██   ██╗   ██║       ██════════╝  
-    ██║         ██     ██╗  ██║       ██║         
-    ██║ ██████╗███████████║ ██║       ██████████╗ 
-    ██║    ║██║██╔═══════██║██║       ██════════╝ 
-    ██╚════╝██║██║       ██║██║       ██║         
-    ██████████║██║       ██║█████████╗██████████╗ 
-    ╚═════════╝╚═╝       ╚═╝╚════════╝╚═════════╝ `);
-
-    // <a id="site-name" href="/">博主来了</a>
-// console.log(document.getElementById('site-name'));
-// document.getElementById('site-name').innerHTML = `<a>哈哈</a>`
-// console.log(document.getElementById("aside-content"))
-// console.log(document.getElementsByClassName('card-widget card-announcement wow animate__zoomIn').constructor.length)
-
-// console.log(document.getElementById('card-info-btn').style)
+console.log("\n    ██████████    █████╗    ██╗       ██████████╗ \n    ██║          ██   ██╗   ██║       ██════════╝  \n    ██║         ██     ██╗  ██║       ██║         \n    ██║ ██████╗███████████║ ██║       ██████████╗ \n    ██║    ║██║██╔═══════██║██║       ██════════╝ \n    ██╚════╝██║██║       ██║██║       ██║         \n    ██████████║██║       ██║█████████╗██████████╗ \n    ╚═════════╝╚═╝       ╚═╝╚════════╝╚═════════╝ ");
